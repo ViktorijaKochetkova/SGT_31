10c47971080bf6f13b3a24bb55706f60e36d2850
Alisa <kochetit@gmail.com>
2022-08-28 22:52:15 +0300

    HW PRS final

--- src/HomeWork/rockPaperScissors/RockPaperScissor.java
+++ src/HomeWork/rockPaperScissors/RockPaperScissor.java
@@ -2,7 +2,7 @@ package HomeWork.rockPaperScissors;
 
 import java.util.Scanner;
 
-public class RockPaperScissor extends Play {
+public class RockPaperScissor extends Play{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter Player`s 1 choice: ");
@@ -10,7 +10,46 @@ public class RockPaperScissor extends Play {
         System.out.println("Please enter Player`s 2 choice: ");
         String player2 = scanner.nextLine().trim().toLowerCase();
         Play play1 = new Play();
-        System.out.println(play1.rockPaperScissors());
+        System.out.println(play1.rockPaperScissors(player1,player2));
+    }
+
+}
+class Play{
+    private String rock = "rock";
+    private String paper = "paper";
+    private String scissors = "scissors";
+
+    public String rockPaperScissors(String player1,String player2) {
+        String win1 = "Player 1 Win!";
+        String win2 = "Player 2 Win!";
+        String tie = "Tie!";
+        String invalid = "Invalid enter";
+        if (player1.equals(player2)) {
+            System.out.println();
+            return tie;
+        } else if (player1.equals(rock) && player2.equals(paper)) {
+            System.out.println();
+            return win2;
+        } else if (player1.equals(paper) && player2.equals(rock)) {
+            System.out.println();
+            return win1;
+        } else if (player1.equals(scissors) && player2.equals(paper)) {
+            System.out.println();
+            return win1;
+        } else if (player1.equals(paper) && player2.equals(scissors)) {
+            System.out.println();
+            return win2;
+        } else if (player1.equals(scissors) && player2.equals(rock)) {
+            System.out.println();
+            return win2;
+        } else if (player1.equals(rock) && player2.equals(scissors)) {
+            System.out.println();
+            return win1;
+        } else {
+            System.out.println();
+            return invalid;
+        }
     }
 }
+
 
